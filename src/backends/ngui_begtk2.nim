36a24dc6405ca986b9823eb3825472a3fb503ea6
WhiteDuke <whiteduke@protonmail.com>
2021-02-24 10:17:44 +0100

    internalSetEvent implemented

--- src/backends/ngui_begtk2.nim
+++ src/backends/ngui_begtk2.nim
@@ -15,6 +15,9 @@ template bError(str: string) =
 type gtk2Window = gtk2.PWindow
 type gtk2Widget = gtk2.PWidget
 type gtk2Container = gtk2.PContainer
+type gtk2TextView = gtk2.PTextView
+type gtk2RadioButton = gtk2.PRadioButton
+type GPointer = PGpointer
 
 proc nextID: NID =
   # ngui_begtk2.nim
@@ -25,24 +28,127 @@ proc nextID: NID =
 
 
 # EVENT -----------------------------------------
+var setEventHandled: bool
+
+proc internalEventHandled =
+  setEventHandled = true
+
+proc triggerEvent(source, data: GPointer): bool {.cdecl.} =
+  let event = cast[NElementEvent](data)
+  if not utilTrigger(event, source):
+    raiseAssert("Event CallBack not found: " & $(event))
+  result = setEventHandled
+  setEventHandled = false
+
 proc internalSetEvent(this: NElement, event: NElementEvent, action: NEventProc) =
-  ## Attach event to this element. Cannot set more than one event per kind
-  # REMOVE BODY AND ADD YOUR OWN IMPLEMENTATION
-  when LAX_ERROR: bInfo("proc internalSetEvent(this: NElement, event: NElementEvent, action: NEventProc)")
-  else: bError("proc internalSetEvent(this: NElement, event: NElementEvent, action: NEventProc)")
+  # Feast your eyes
+  # https://developer.gnome.org/gtk3/stable/GtkWidget.html#GtkWidget.signals
+  const EVENT_TO_SIGNAL = [
+    neNone: "LET'S_DANCE", neClick: "button-press-event",
+    neClickRelease: "button-release-event", neMove: "motion-notify-event",
+    neEnter: "activate",
+    neChange: "changed", neOpen: "popped-up", neFocusOn: "focus-in-event",
+    neFocusOff: "focus-out-event", neDESTROY: "destroy", neSHOW: "show",
+    neHIDE: "hide", neKeyPress: "key_press_event",
+    neKeyRelease: "key-release-event",
+  ]
+  
+  var
+    nguiEvent   = event
+    nguiTrigger = SIGNAL_FUNC(triggerEvent)
+    gtkInst     = cast[gtk2Widget](this.raw)
+    gtkEvent    = EVENT_TO_SIGNAL[nguiEvent]
+    gtkData     = cast[GPointer](nguiEvent)
+
+    #discard this.data(gtk2Window).signalConnect(
+    #"destroy", SIGNAL_FUNC(cb), nil)
+
+  # -----------------------------------
+  # Hack List, insert your hacks here -
+
+  # REASON: Special user function
+  if not (this of Button) and
+      nguiEvent in {neFocusOff, neFocusOn, neKeyPress,
+                   neKeyRelease, neClick, neClickRelease, neMove}:
+    nguiTrigger = SIGNAL_FUNC(
+      proc(s, _, d: GPointer): bool {.cdecl.} =
+        triggerEvent(s, d)
+    )
+  
+  # REASON: Hard to explain
+  if this of Image and nguiEvent in {neClick, neClickRelease, neMove}:
+    # https://developer.gnome.org/gtk3/stable/GtkImage.html#GtkImage.description
+    # "Handling button press events on a GtkImage."
+    # TODO gtkInst  = cast[gtk2Widget](utilInsertAdapter(gtkInst, adaptersEventBox))
+    doAssert false
+
+  # REASON: Different name
+  if this of FileChoose and nguiEvent == neClick:
+    # https://developer.gnome.org/gtk3/stable/GtkDialog.html#GtkDialog-response
+    gtkEvent = "response"
+  
+  elif this of Button and nguiEvent == neClick:
+    # https://developer.gnome.org/gtk3/stable/GtkButton.html#GtkButton-clicked
+    gtkEvent = "clicked"
+  
+  elif this of Slider and nguiEvent == neChange:
+    # https://developer.gnome.org/gtk3/stable/GtkRange.html#GtkRange-value-changed
+    gtkEvent = "value-changed"
+
+  elif this of Checkbox and nguiEvent == neChange:
+    # https://developer.gnome.org/gtk3/stable/GtkToggleButton.html#GtkToggleButton-toggled
+    gtkEvent = "toggled"
+  
+  elif this of List and nguiEvent in {neChange}:
+    # https://developer.gnome.org/gtk3/stable/GtkListBox.html#GtkListBox-selected-rows-changed
+    gtkEvent = "selected-rows-changed"
+
+  elif this of Calendar and nguiEvent in {neChange}:
+    # https://developer.gnome.org/gtk3/stable/GtkCalendar.html#GtkCalendar-day-selected
+    gtkEvent = "day-selected"
+
+  # REASON: Event applies only to TextBuffer
+  elif this of TextArea and nguiEvent in {neChange}:
+    # https://developer.gnome.org/gtk3/stable/GtkTextView.html#gtk-text-view-get-buffer
+    # https://developer.gnome.org/gtk3/stable/GtkTextBuffer.html#GtkTextBuffer-changed
+    gtkInst = cast[gtk2Widget](gtk2TextView(gtkInst).getBuffer())
+
+  # REASON: onClick is triggered for old/new radio
+  elif this of Radio and nguiEvent in {neClick}:
+    nguiTrigger = SIGNAL_FUNC(proc(source, data: GPointer): bool {.cdecl.} =
+      if bool(cast[gtk2RadioButton](source).getActive()):
+        return triggerEvent(source, data))
+
+  # REASON: Labels don't really have click events, but we need this for menus
+  elif this of Label and nguiEvent in {neClick}: # TODO: Handle onClick for labels outside menus
+    # https://developer.gnome.org/gtk3/stable/GtkMenuItem.html#GtkMenuItem-activate
+    gtkEvent = "activate"
+    doAssert false
+    #TODO gtkInst  =
+      #cast[gtk2Widget](utilInsertAdapter(gtkInst, adaptersMenuItem))
+  
+  # REASON: Snowflake callback, and the first time is triggered twice
+  elif this of Menu and nguiEvent in {neOpen}:
+    # https://developer.gnome.org/gtk3/stable/GtkMenu.html#GtkMenu-popped-up
+    nguiTrigger = SIGNAL_FUNC(
+        proc(
+          source, b, c: GPointer,
+          d, e: bool,
+          data: GPointer): bool {.cdecl.} =
+      once: return # Why?!
+      return triggerEvent(source, data))
+
+  # -------------------------------------
+  doAssert not utilExists(nguiEvent, gtkInst) # TODO: replace old event with new one. I added this to debug a bug.
+  utilSet(nguiEvent, gtkInst, action)
+  # https://developer.gnome.org/gobject/stable/gobject-Signals.html#g-signal-connect
+  discard signalConnect(gtkInst, gtkEvent, nguiTrigger, gtkData)
 
 proc internalGetCurrentEvent: NEventArgs =
   ## Get the args from the event that is currently being dispatched
   # REMOVE BODY AND ADD YOUR OWN IMPLEMENTATION
   when LAX_ERROR: bInfo("proc internalGetCurrentEvent: NEventArgs")
   else: bError("proc internalGetCurrentEvent: NEventArgs")
-
-proc internalEventHandled() =
-  ## Stop event propagation
-  # REMOVE BODY AND ADD YOUR OWN IMPLEMENTATION
-  when LAX_ERROR: bInfo("proc internalEventHandled()")
-  else: bError("proc internalEventHandled()")
-
 
 
 # WIDGET ----------------------------------------
@@ -255,7 +361,7 @@ proc internalStop(this: App) =
 # WINDOW ----------------------------------------
 proc onDestroyWin(this: Window) =
   # ngui_begtk3
-  proc cb(this: gtk2Widget, data: PGpointer) {.cdecl.} =
+  proc cb(this: gtk2Widget, data: GPointer) {.cdecl.} =
     if utilLen(pApp) == 1: internalStop(pApp)
   discard this.data(gtk2Window).signalConnect(
     "destroy", SIGNAL_FUNC(cb), nil)
